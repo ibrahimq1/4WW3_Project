7c50cfca69ca6b3d1d62f2edacf742a32569b3ea
Frank Su <franksu49@gmail.com>
2021-12-01 15:27:41 -0500

    fdsklfjusdalkf

--- Project_Part_3/index.php
+++ Project_Part_3/index.php
@@ -81,10 +81,10 @@
 								<div class="input-group-text">
 									<select data-trigger="" name="rating">
 										<option placeholder="" value="">Rating</option>
-										<option>1+</option>
-										<option>2+</option>
-										<option>3+</option>
-										<option>4+</option>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
 										<option>5</option>
 									</select>
 								</div>
--- Project_Part_3/scripts/retrieve_courts.php
+++ Project_Part_3/scripts/retrieve_courts.php
@@ -19,7 +19,42 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
       $_SESSION["flash-error"] = ["message" => "Please enter either a rating or latitude/longitude to search by!"];
       header("Location: /Project_Part_3/index.php");
       die();
-    } else if (empty($location) && !empty($rating)) {
+    }
+
+    // make a connection to database
+    // create DB connection using secrets in .env file
+    $databasename = getenv('DB_NAME');
+    $dbhost = getenv('DB_HOST');
+    $dbuser = getenv('DB_USER');
+    $dbpassword = getenv('DB_PASSWORD');
+    $conn = new mysqli($dbhost, $dbuser, $dbpassword, $databasename);
+
+    // error connecting to database
+    if ($conn->connect_error) {
+      if (isset($_SESSION["flash-error"])) {
+        unset($_SESSION["flash-error"]);
+      }
+      $_SESSION["flash-error"] = ["message" => "Error connecting to database!"];
+      header("Location: /Project_Part_3/index.php");
+      die();
+    }
+
+    // if user searched by rating only, 
+    if (empty($location) && !empty($rating)) {
+      $retrieveCourtsSql = "SELECT id FROM submitted_courts WHERE rating>=" . $rating;
+    }
+
+    // get a list of courtIds that satisfy the search query
+    if ($result = $conn->query($retrieveCourtsSql)) {
+
+      $courtIdArray = array();
+
+      // loop through result object, populate array with rows
+      while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $courtIdArray[] = $row['id'];
+      }
+
+      
     }
   }
 } else {
