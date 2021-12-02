3b06b45a046f6351741bc95c691967b1a54b8f58
Frank Su <franksu49@gmail.com>
2021-12-02 14:42:54 -0500

    searching now works by distance as well

--- Project_Part_3/scripts/retrieve_courts.php
+++ Project_Part_3/scripts/retrieve_courts.php
@@ -44,6 +44,29 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
       $retrieveCourtsSql = "SELECT id FROM submitted_courts WHERE rating>=" . $rating;
     }
 
+    // if user searched by location only
+    else if (empty($rating) && !empty($location)) {
+
+      // if location was not entered in correct format, redirect to homepage with error message
+      if (!preg_match("/^(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)$/", $location)) {
+        if (isset($_SESSION["flash-error"])) {
+          unset($_SESSION["flash-error"]);
+        }
+        $_SESSION["flash-error"] = ["message" => "Please make sure location is in the format: longitude, latitude (seperate longitude and latitude with a comma)"];
+        header("Location: /Project_Part_3/index.php");
+        die();
+      }
+
+      // if location entered correctly, we query database for all courts within 50 km of starting location
+      else {
+        $locationNoWhitespace = str_replace(' ', '', $location);
+        $startingLatitude = explode(",", $locationNoWhitespace)[0];
+        $startingLongitude = explode(",", $locationNoWhitespace)[1];
+        $retrieveCourtsSql = "SELECT id, SQRT(POW(69.1 * (latitude - " . $startingLatitude . "), 2) + POW(69.1 * (" . $startingLongitude . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM submitted_courts HAVING distance < 50 ORDER BY distance";
+      }
+    }
+
+
     // get a list of courtIds that satisfy the search query
     if ($result = $conn->query($retrieveCourtsSql)) {
 
@@ -54,18 +77,34 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         }
         $_SESSION["flash-success"] = ["message" => "Displaying all courts with rating greater than or equal to " . $rating];
       }
+      // set success flash message if user was seasrching only by location
+      else if (empty($rating) && !empty($location)) {
+        if (isset($_SESSION["flash-success"])) {
+          unset($_SESSION["flash-success"]);
+        }
+        $_SESSION["flash-success"] = ["message" => "Displaying all courts less than 50km away from latitude:  " . $startingLatitude . " and longitude: " . $startingLongitude];
+      }
 
       $courtIdArray = array();
+      $distanceArray = array();
 
       // loop through result object, populate array with rows
       while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
         $courtIdArray[] = $row['id'];
+        if (isset($row['distance'])) {
+          $distanceArray[] = $row['distance'];
+        }
       }
 
       // set session variable with the array of courtIds
       if (isset($_SESSION["courtsToDisplay"])) {
         unset($_SESSION["courtsToDisplay"]);
       }
+      if (isset($_SESSION["courtDistances"])) {
+        unset($_SESSION["courtDistances"]);
+      }
+
+      $_SESSION['courtDistances'] = $distanceArray;
       $_SESSION['courtsToDisplay'] = $courtIdArray;
 
       header("Location: /Project_Part_3/search_results.php");
